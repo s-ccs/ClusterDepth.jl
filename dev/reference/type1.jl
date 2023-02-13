4cf888eba0fedf8faa17f79eaa7d62f2dda60427
Documenter.jl <documenter@juliadocs.github.io>
2023-02-13 21:00:25 +0000

    build based on 8321510

--- dev/reference/type1.jl
+++ dev/reference/type1.jl
@@ -9,8 +9,8 @@ using DataFrames
 # ### Family Wise Error of ClusterDepth
 # Here we calculate the Family Wise Error the ClusterDepth Correct
 # That is, we want to check that pvalues we get, indeed return us a type-1 of 5% for all time-points
-#
-# The point being, that if you do tests on 200 timepoints, the chance that one is significant is actually $(@show 1-(1-0.05)^113) and not 5%
+# The point being, that if you do tests on 113 timepoints, the chance that one is significant is not 5% but 
+(1-(1-0.05)^113)*100 ##%
 
 # Let's setup a simulation using UnfoldSim.jl. We simulate a simple 1x2 design with 20 subjects
 n_subjects=20
@@ -32,7 +32,7 @@ signal = MixedModelComponent(;
 function run_fun(r)
     data, events = simulate(MersenneTwister(r), design, signal, UniformOnset(; offset=5, width=4), RedNoise(noiselevel=1); return_epoched=true)
     data = data[:,events.condition .== "small"] .- data[:,events.condition .== "large"]
-    return data,clusterdepth(data; τ=quantile(TDist(n_subjects - 1), 0.95),nperm=10000)
+    return data,clusterdepth(data; τ=quantile(TDist(n_subjects - 1), 0.95),nperm=1000)
 end;
 
 #----
@@ -60,17 +60,17 @@ f
 #----
 # ## run simulation
 # This takes some seconds (depending on your infrastructure)
-reps = 100
+reps = 500
 res = fill(NaN, reps, 2)
 Threads.@threads for r = 1:reps
     data,pvals = run_fun(r)
-    res[r, 1] = mean(pvals .<= 0.025)
+    res[r, 1] = mean(pvals .<= 0.05)
     res[r, 2] = mean(abs.(ClusterDepth.studentt(data)) .>= quantile(TDist(n_subjects - 1), 0.975))
 end;
 # Finally, let's calculate the percentage of simulations where we find a significant effect somewhere
 mean(res.>0,dims=1) |> x-> (:clusterdepth=>x[1],:uncorrected=>x[2])
 
-# Nice, correction seems to work fine :) It is not exactly 0.05, but with more repetitions we should get there.
+# Nice, correction seems to work in principle :) Clusterdepth is not exactly 5%, but with more repetitions we should get there (e.g. with 5000 repetitions, we get 0.051%).
 
 # !!! info
 #       if you look closely, the `:uncorrected` value (around 60%) is not as bad as the 99% promised in the introduction. This is due to the correlation between the tests introduced by the noise. Indeed, a good exercise is to repeat everything, but put `RedNoise` to `WhiteNoise`
