30aaad4013acffa0a78c002d4af5d1d93eaa78b4
maanikmarathe <66105649+maanikmarathe@users.noreply.github.com>
2023-07-03 17:55:45 +0200

    updated sign_permute and its tests for input with >2 dimensions

--- src/utils.jl
+++ src/utils.jl
@@ -4,9 +4,19 @@ studentt(x::AbstractMatrix) = (mean(x,dims=2)[:,1])./(std(x,dims=2)[:,1]./sqrt(s
 
 """
 Permutation via random sign-flip
-Flips signs along the second dimension
+Flips signs along the last dimension
 """
-function sign_permute(rng,x::AbstractMatrix,fun) 
-	binflip=repeat(((rand(rng,size(x,2)).>0.5).*2).-1, 1,size(x,1))'
-	return fun(x.*binflip)
+function sign_permute(rng,x::AbstractArray,fun) 
+	n = ndims(x)
+	@assert n > 1 "vectors cannot be permuted"
+	
+	fl = rand(rng,[-1,1],size(x,n))
+	flipped = map((x,y)->x.*y,eachslice(x;dims=n),fl)
+
+	y = similar(x)
+	for (ix,sl) = enumerate(eachslice(y;dims=n))
+		sl.=flipped[ix]
+	end 
+
+	return fun(y)
 end
