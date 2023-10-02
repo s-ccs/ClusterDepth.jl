47056fd5d54064d26c9dff7d97a9a7a6a6ee3c7a
behinger (s-ccs 001) <benedikt.ehinger@vis.uni-stuttgart.de>
2023-10-02 08:57:26 +0000

    old fixes?

--- docs/src/tutorials/eeg.jl
+++ docs/src/tutorials/eeg.jl
@@ -21,20 +21,20 @@ first(generate(design),5)
 # let's simulate a P100, a N170 and a P300 - but an effect only on the N170
 p100 = MixedModelComponent(;
     basis=UnfoldSim.p100(;sfreq=250),
-    formula=@formula(dv ~ 1+(1|subject)),
+    formula=@formula(0 ~ 1+(1|subject)),
     β=[1.],
     σs = Dict(:subject=>[1]),
 );
 n170 = MixedModelComponent(;
     basis=UnfoldSim.n170(;sfreq=250),
-    formula=@formula(dv ~ 1+condition+(1+condition|subject)),
+    formula=@formula(0 ~ 1+condition+(1+condition|subject)),
     β=[1.,-0.5], # condition effect - faces are more negative than cars
     σs = Dict(:subject=>[1,0.2]), # random slope yes please!
 );
 
 p300 = MixedModelComponent(;
     basis=UnfoldSim.p300(;sfreq=250),
-    formula=@formula(dv ~ 1+condition+(1+condition|subject)),
+    formula=@formula(0 ~ 1+condition+(1+condition|subject)),
     β=[1.,0], ## no p300 condition effect
     σs = Dict(:subject=>[1,1.]), # but a random slope for condition
 );
@@ -47,6 +47,8 @@ times = range(0,stop=size(data,1)/250,length=size(data,1));
 allData = hcat(events,DataFrame(:eeg=>[[col...] for col in eachcol(data)]));
 
 # let's fit an Unfold Model for each subject
+# !!! note
+#       In principle, we do not need Unfold here - we could simply calculate (subjectwise) means of the conditions, and their time-resolved difference. Using Unfold.jl here simply generalizes it to more complex design, e.g. with continuous predictors etc.
 models = groupby(allData,:subject) |> 
     x->combine(x,AsTable(:)=>(oneSubject->fit(UnfoldModel,
                                             @formula(0~1+condition),
@@ -59,11 +61,11 @@ models = groupby(allData,:subject) |>
 
 allEffects = combine(groupby(models,:subject),
     :unfoldModel=>(x->effects(Dict(:condition=>["car","face"]),x[1]))=>AsTable)
-plot_erp(allEffects;setMappingValues=(:color=>:condition,:group=>:subject))
+plot_erp(allEffects;mapping=(:color=>:condition,:group=>:subject))
 
 # extract the face-coefficient from the linear model
 allCoefs = combine(groupby(models,:subject),:unfoldModel=>(x->coeftable(x[1]))=>AsTable)
-plot_erp(allCoefs;setMappingValues=(:group=>:subject,col=:coefname))
+plot_erp(allCoefs;mapping=(:group=>:subject,col=:coefname))
 
 # let's unstack the tidy-coef table into a matrix and put it to clusterdepth for clusterpermutation testing
 faceCoefs = allCoefs|>x->subset(x,:coefname=>x->x.=="condition: face")
@@ -83,7 +85,7 @@ faceERP = groupby(faceCoefs,[:time,:coefname])|>
                  :estimate=>std=>:stderror);
 
 # put the pvalues into a nicer format (in the future UnfoldMakie should do)
-plot_erp(faceERP; setExtraValues=(;stderror=true))
+plot_erp(faceERP; extra=(;stderror=true))
 significant = (pvals.<=0.05) |>allowmissing
 significant[significant.==0] .= missing
 lines!(times,significant.*-1,color=:darkblue,linewidth=4)
