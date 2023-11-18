3e7f32ff54bf23cb2284fcf645f9653129fd94b4
Benedikt Ehinger <benedikt.ehinger@vis.uni-stuttgart.de>
2023-11-19 00:06:14 +0100

    update to inplace permutation

--- test/utils.jl
+++ test/utils.jl
@@ -1,29 +1,29 @@
 @testset "sign_permute" begin
 
     m = [1 1 1;2 2 2;3 3 3;4 4 4]
-    p = ClusterDepth.sign_permute(StableRNG(1),m,x->x)
+    p = ClusterDepth.sign_permute!(StableRNG(1),m,x->x)
     
     @test p[1,:] == [1, -1, -1]
 
     # different seeds are different
-    @test p!= ClusterDepth.sign_permute(StableRNG(3),m,x->x)
+    @test p!= ClusterDepth.sign_permute!(StableRNG(3),deepcopy(m),x->x)
     # same seeds are the same
-    @test p == ClusterDepth.sign_permute(StableRNG(1),m,x->x)
+    @test p == ClusterDepth.sign_permute!(StableRNG(1),deepcopy(m),x->x)
 
     m = ones(1,1000000)
-    @test abs.(ClusterDepth.sign_permute(StableRNG(1),m,mean))<0.001
+    @test abs.(ClusterDepth.sign_permute!(StableRNG(1),deepcopy(m),mean))<0.001
 
     m = ones(1,2,3,4,5,6,7,100)
-    o = ClusterDepth.sign_permute(StableRNG(1),m,x->x)
+    o = ClusterDepth.sign_permute!(StableRNG(1),deepcopy(m),x->x)
     @test sort(unique(mean(o,dims=1:ndims(o)-1))) == [-1.,1.]
     
     #2D input data
     data = randn(StableRNG(1),4,5)
-    @test size(ClusterDepth.sign_permute(StableRNG(1),data,ClusterDepth.studentt)) == (4,)
+    @test size(ClusterDepth.sign_permute!(StableRNG(1),data,ClusterDepth.studentt)) == (4,)
 
     #3D input data
     data = randn(StableRNG(1),3,4,5);
-    @test size(ClusterDepth.sign_permute(StableRNG(1),data,ClusterDepth.studentt)) == (3,4) 
+    @test size(ClusterDepth.sign_permute!(StableRNG(1),data,ClusterDepth.studentt)) == (3,4) 
     
 end
 
