484c3ac615caaa6b4c9768ac3a889af78358772d
behinger (s-ccs 001) <benedikt.ehinger@vis.uni-stuttgart.de>
2023-11-22 21:40:14 +0000

    refactored - breaking - improved ttest performance, inplace ttest

--- src/utils.jl
+++ src/utils.jl
@@ -1,21 +1,73 @@
+function studentt!(out::AbstractMatrix,x::AbstractArray{<:Real,3};kwargs...)
+	
+	for (x_ch,o_ch) = zip(eachslice(x,dims=1),eachslice(out,dims=1))
+		#@debug size(x_ch),size(o_ch)
+		studentt!(o_ch,x_ch;kwargs...)
+	end
+	return out
+end
 
-studentt(x::AbstractMatrix) = (mean(x,dims=2)[:,1])./(std(x,dims=2)[:,1]./sqrt(size(x,2)-1))
 
-studentt(x::AbstractArray{<:Real,3}) = return dropdims(mapslices(studentt,x,dims=(2,3)),dims=3)
+"""
+	studentt_test!(out,x;type=:abs)
+
+strongly optimized one-sample t-test function.
+
+
+Implements: t =  mean(x) / ( sqrt(var(x))) / sqrt(size(x,2)-1)
+
+Accepts 2D or 3D matrices, always aggregates over the last dimension
+
+"""
+function studentt_test!(out,x::AbstractMatrix)
+	mean!(out,x)
+	df = 1 ./ sqrt(size(x,2)-1)
+#@debug size(out),size(x)
+tmp = [1.]
+for k = eachindex(out)
+		std!(tmp,@view(x[k,:]),out[k])
+		@views out[k] /= (sqrt(tmp[1]) * df)
+	end
+ 	return out
+end
+
+function std!(tmp,x_slice,μ)
+
+		@views x_slice .= (x_slice .- μ).^2 
+		sum!(tmp,x_slice)
+		tmp .= sqrt.(tmp ./ (length(x_slice)-1))
+		
+		
+end
+
+
+function studentt!(out,x)
+	#@debug size(out),size(x)
+	mean!(out,x)
+	out .= out ./ (std(x,mean=out, dims=2)[:,1]./sqrt(size(x,2)-1))
+end
+function studentt(x::AbstractMatrix)
+	# more efficient than this one liner
+	# studentt(x::AbstractMatrix) = (mean(x,dims=2)[:,1])./(std(x,dims=2)[:,1]./sqrt(size(x,2)-1))
+	μ = mean(x,dims=2)[:,1]
+	μ .= μ ./ (std(x,mean=μ, dims=2)[:,1]./sqrt(size(x,2)-1))
+end
+
+studentt(x::AbstractArray{<:Real,3}) = dropdims(mapslices(studentt,x,dims=(2,3)),dims=3)
 
 """
 Permutation via random sign-flip
 Flips signs along the last dimension
 """
-function sign_permute!(rng,x::AbstractArray,fun) 
+function sign_permute!(rng,x::AbstractArray) 
 	n = ndims(x)
 	@assert n > 1 "vectors cannot be permuted"
 	
 	fl = rand(rng,[-1,1],size(x,n))
-	#flipped = map((x,y)->x.*y,eachslice(x;dims=n),fl)
-	for (f,k) = zip(fl,eachslice(x;dims=n))
-		k .*= f
+
+	for (flip,xslice) = zip(fl,eachslice(x;dims=n))
+		xslice .= xslice .* flip
 	end
 
-	return fun(x)
+	return x
 end
--- test/utils.jl
+++ test/utils.jl
@@ -1,30 +1,23 @@
 @testset "sign_permute" begin
 
     m = [1 1 1;2 2 2;3 3 3;4 4 4]
-    p = ClusterDepth.sign_permute!(StableRNG(2),deepcopy(m),x->x)
+    p = ClusterDepth.sign_permute!(StableRNG(2),deepcopy(m))
     
     @test p[1,:] == [1, -1, 1]
 
     # different seeds are different
-    @test p!= ClusterDepth.sign_permute!(StableRNG(3),deepcopy(m),x->x)
+    @test p!= ClusterDepth.sign_permute!(StableRNG(3),deepcopy(m))
     # same seeds are the same
-    @test p == ClusterDepth.sign_permute!(StableRNG(2),deepcopy(m),x->x)
+    @test p == ClusterDepth.sign_permute!(StableRNG(2),deepcopy(m))
 
     m = ones(1,1000000)
-    @test abs.(ClusterDepth.sign_permute!(StableRNG(1),deepcopy(m),mean))<0.001
+    @test abs(mean(ClusterDepth.sign_permute!(StableRNG(1),deepcopy(m))))<0.001
 
     m = ones(1,2,3,4,5,6,7,100)
-    o = ClusterDepth.sign_permute!(StableRNG(1),deepcopy(m),x->x)
+    o = ClusterDepth.sign_permute!(StableRNG(1),deepcopy(m))
     @test sort(unique(mean(o,dims=1:ndims(o)-1))) == [-1.,1.]
     
-    #2D input data
-    data = randn(StableRNG(1),4,5)
-    @test size(ClusterDepth.sign_permute!(StableRNG(1),data,ClusterDepth.studentt)) == (4,)
 
-    #3D input data
-    data = randn(StableRNG(1),3,4,5);
-    @test size(ClusterDepth.sign_permute!(StableRNG(1),data,ClusterDepth.studentt)) == (3,4) 
-    
 end
 
 @testset "studentt" begin
@@ -37,6 +30,24 @@ end
     #2D input data
     data = randn(StableRNG(1),4,5)
     @test size(ClusterDepth.studentt(data)) == (4,)
+
+    #3D input data
+    data = randn(StableRNG(1),3,4,5);
+    @test size(ClusterDepth.studentt(data)) == (3,4) 
+
+    #
+    t = rand(10000)
+    ClusterDepth.studentt!(t,x)
+    @test t ≈ ClusterDepth.studentt(x)
+    @test length(t) == 10000
+    @test maximum(abs.(t))<10 # we'd need to be super lucky ;)
+    @test mean(abs.(t).>2) < 0.06
+
+    #2D input data
+    data = randn(StableRNG(1),4,5)
+    t = rand(4)
+    ClusterDepth.studentt!(t,data)
+    @test size(t) == (4,)
 
     #3D input data
     data = randn(StableRNG(1),3,4,5);
